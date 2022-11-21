876380fdf35f84776f578eccf8c93a1914f5de59
inforgstr <xboyxmax@gmail.com>
2022-11-21 20:36:31 +0500

    upd: Completed and added third task of codewars -> HW3

--- Semester_1/lesson_21/hw_3.py
+++ Semester_1/lesson_21/hw_3.py
@@ -22,3 +22,16 @@ def solve(s):
     elif len(lowers) < len(uppers):
         return s.upper()
 print(solve('GooFDl'))
+
+
+# code of third task codewars
+def arithmetic(a, b, operator):
+    if operator == 'add':
+        return a + b
+    elif operator == 'subtract':
+        return a - b
+    elif operator == 'multiply':
+        return a * b
+    else:
+        return a / b
+print(arithmetic(1, 2, 'subtract'))
