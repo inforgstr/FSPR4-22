ac223665ac3beec45ba3396b2ece5262f447bd97
inforgstr <xboyxmax@gmail.com>
2022-11-24 18:01:56 +0500

    added 3 lesson of HW lesson_22.py

--- Semester_1/lesson_22/hw_of_lesson22.py
+++ Semester_1/lesson_22/hw_of_lesson22.py
@@ -18,7 +18,7 @@ class Car:
         if fl < self.Fuel_way/10:
             return f'{fl*10} км пройденный путь \n{self.Fuel_way - fl*10} км оставшийся путь'
         elif fl > self.Fuel_way/10:
-            return f'{fl*10} км пройденный путь'
+            return f'{fl*10} км пройденный путь\n'
     
 car = Car('BMW', 'blue', '70000 $', 'Germany', 'N57D30S1')
 print("Car's characteristic:", "\nModel:", car.model, "\nColor:", car.color, 
@@ -26,3 +26,17 @@ print("Car's characteristic:", "\nModel:", car.model, "\nColor:", car.color,
     '\nEngine:', car.engine, '\n\n')
 print(car.acceleration(12))
 print(car.fuel(40))
+
+# 3 task 
+# Создать класс описывающий человека с методами которые описывают вас (что умеете)
+class Human:
+    def head(self):
+        return 'Head can think'
+    def legs(self):
+        return 'Legs can run'
+    def hands(self):
+        return 'Hands can write'
+human = Human()
+print(human.head())
+print(human.legs())
+print(human.hands())
