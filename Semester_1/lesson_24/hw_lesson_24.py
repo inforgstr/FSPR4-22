e9b668020c3780e3e415f5d39786a3c153e09ce3
inforgstr <xboyxmax@gmail.com>
2022-11-28 21:04:10 +0500

    Added completed task 2 from H/W lesson_24

--- Semester_1/lesson_24/hw_lesson_24.py
+++ Semester_1/lesson_24/hw_lesson_24.py
@@ -16,3 +16,32 @@ class Functions:
 func = Functions()
 print(func.area_rectangle(208, 56), func.area_triangle(4, 5, 6))
 print(func.power(45, 7), func.multiply(78, 45))
+
+# 2. Создать класс, который описывает игрового персонажа с атрибутами (health, mana, damage, type, skill) и методами attack, heal, level_up 
+# от которого наследуются 3 класса Archer, Paladin, Wizard. На свою фантазию можете расширить функциональность 
+class Player:
+    def __init__(self, health, mana, damage, type, skill):
+        self.health = health
+        self.mana = mana
+        self.damage = damage
+        self.type = type
+        self.skill = skill
+    def attack(self):
+        return self.damage
+    def heal(self):
+        return self.health
+    def level_up(self):
+        if 20 < self.health and self.mana > 20 and self.damage > 20:
+            return 'LEVEL UP!'
+
+class Archer(Player):
+    pass
+class Paladin(Player):
+    pass
+class Wizard(Player):
+    pass
+player = Paladin(34, 23, 345, 'afd', 'yo')
+player1 = Archer(40, 76, 50, 'af', 'high')
+print('Атака персонажа(Archer)', player1.attack())
+print(f'Здоровье персонажа(Paladin) {player.heal()}')
+
