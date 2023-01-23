204537ca9358e793e3847dd99745872f6d92c48e
inforgstr <xboyxmax@gmail.com>
2023-01-23 23:41:32 +0500

    upgrades on interface

--- Semester_2/Lesson_1/lesson_1.py
+++ Semester_2/Lesson_1/lesson_1.py
@@ -6,24 +6,25 @@ user_points = 0
 # Test explanation
 print(
     f"Hello there!\nInput correct answer, for skip questions use button 'E' with upper case.\nThere are some GPT system requirements:\n"
-    f"\n\tA - greater than 86%\n\tB - greater than 75%\n\tC - greater than 65%\n\tFor F - less than 65%.\n\t\t\t\tGood Luck!"
+    f"\n\tA - greater than 86%\n\tB - greater than 75%\n\tC - greater than 65%\n\tFor F - less than 65%.\n\t\t\t\t\t\t\tGood Luck!"
 )
 
 # Itering question for test user
-for question, val in questions.items():
-    new_question = input(f"\n{question} \n")
+if questions:
+  for question, val in questions.items():
+      new_question = input(f"\n{question} \n")
+  
+      if new_question == val:
+          user_results.append("correct")
+          user_points += 1
+      elif new_question == "E":
+          user_results.append("skipped")
+          user_points += 0
+      else:
+          user_results.append("incorrect")
+          user_points += 0
 
-    if new_question == val:
-        user_results.append("correct")
-        user_points += 1
-    elif new_question == "E":
-        user_results.append("skipped")
-        user_points += 0
-    else:
-        user_results.append("incorrect")
-        user_points += 0
-
-percent = abs(user_points * (100 / len(questions)))
+percent = user_points * (100 / len(questions))
 
 """GPA system"""
 if percent > 86:
@@ -38,12 +39,13 @@ else:
 
 # Output
 print(
-    "|------------------------------------|\n"
-    f"|**********TEST-RESULTf**********|"
-    f"\nYour correct answers: {user_results.count('correct')}."
-    f"\nIncorrect answers: {user_results.count('incorrect')}."
-    f"\nOverall percentage: {percent:.2f}% .\n"
+    "\n\t\t|--------------------------------|\n"
+    f"\t\t|**********TEST-RESULTf**********|\n"
+    f"\n\t\tYour correct answers: {user_results.count('correct')}."
+    f"\n\t\tIncorrect answers: {user_results.count('incorrect')}."
+    f"\n\t\tOverall percentage: {percent:.2f}% .\n"
+    f"\t\t__________________________________\n"
 )
 if user_results:
     for index, answer in enumerate(user_results, 1):
-        print(f"{index} question was {answer}!")
+        print(f"\t\t\t{index} question was {answer}!")
