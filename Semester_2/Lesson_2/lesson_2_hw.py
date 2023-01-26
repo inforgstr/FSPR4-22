4c0880b4787bac1b9fd16c3d6e158e5141b38b16
inforgstr <xboyxmax@gmail.com>
2023-01-26 18:01:54 +0500

    updated

--- Semester_2/Lesson_2/lesson_2_hw.py
+++ Semester_2/Lesson_2/lesson_2_hw.py
@@ -1,18 +1,43 @@
-# lyric = "Ryhana's lyric"           good -> GOOD       monster -> *******
-# monster = "***"
-# count it words, if your input great than 3 ban this user
-# прежде этого login and password.
+DATA = [
+    "user",
+    "password",
+]
 
-def replacer(text, illegal):
-    new_text = text.split()
-    for word in new_text:
-        if word :
-            pass
-
-lyric = ""
+legals = ["am", "good"]
 illegals = ["monster", "bad"]
-legals = ["good", "am"]
+
+
+def replacer(lyric, legals="", illegals=""):
+    res = lyric.split()
+    s = []
+    for i in range(len(res)):
+        if "," not in res[i] and "." not in res[i] and "!" not in res[i]:
+            s.append(res[i])
+            s.append(" ")
+        elif "," in res[i]:
+            s.append(res[i][:-1])
+            s.append(", ")
+        elif "." in res[i]:
+            s.append(res[i][:-1])
+            s.append(". ")
+        elif "!" in res[i]:
+            s.append(res[i][:-1])
+            s.append("! ")
+
+    for i in range(len(s)):
+        if s[i].lower() in illegals:
+            s[i] = len(s[i]) * "*"
+        elif s[i].lower() in legals:
+            s[i] = s[i].upper()
+    return "".join(s)
+
 
 login = input("Input your login: ")
 password = input("Input your password: ")
 
+if login == DATA[0] and password == DATA[1]:
+    print("You have succesfully loged in!")
+    chat = input("Input a text here: ")
+    print(replacer(chat, legals, illegals))
+else:
+    print("Please try again!")
