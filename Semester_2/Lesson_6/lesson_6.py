e52125183547122eecd798b6b6a92793a64a9467
inforgstr <xboyxmax@gmail.com>
2023-03-10 20:01:20 +0500

    updates

---
+++ Semester_2/Lesson_6/lesson_6.py
@@ -0,0 +1,29 @@
+# colors = ["Red", "Black", "White"]
+# def writeToFile(title):
+#     with open(title+".txt", "w") as file:
+#         file.write(title)
+# for color in colors:
+#     writeToFile(color)
+
+# try:
+#     file = open("text.txt", "w")
+#     print(file.read())
+# except:
+#     print("Exception!")
+
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Len[hn2006]",
+    database="mydatabase",
+)
+
+mycursor = mydb.cursor()
+
+
+
+for x in mycursor:
+    print(x)
