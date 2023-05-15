5a9c37efadce11850b203e2f8d10fe83a085bc2c
inforgstr <xboyxmax@gmail.com>
2023-05-15 16:56:49 +0500

    format code

--- Semester_2/Lesson_19/lesson_19hw.py
+++ Semester_2/Lesson_19/lesson_19hw.py
@@ -9,36 +9,38 @@ HW:
 # Problem for 6kyu - problem name `+1 Array`:
 # https://www.codewars.com/kata/5514e5b77e6b2f38e0000ca9/train/python
 
+
 def up_array(arr: list[int]) -> list[int]:
     if not arr:
         return None
     hint = 0
 
-    i = len(arr)-1
-    while i!=-1:
-        if len(str(arr[i]))==2 or "-" in str(arr[i]):
+    i = len(arr) - 1
+    while i != -1:
+        if len(str(arr[i])) == 2 or "-" in str(arr[i]):
             return None
         else:
-            if arr[i]+1==10:
-                if i==arr.index(arr[0]):
-                    if len(arr[i:])==hint+1:
+            if arr[i] + 1 == 10:
+                if i == arr.index(arr[0]):
+                    if len(arr[i:]) == hint + 1:
                         arr[i] = 0
                         arr.insert(0, 1)
                     else:
                         arr[0] = arr[i]
 
-                elif i!=arr.index(arr[0]) and len(arr[i:])==hint+1:
+                elif i != arr.index(arr[0]) and len(arr[i:]) == hint + 1:
                     arr[i] = 0
                     hint += 1
             else:
-                if len(arr[i:])==hint+1:
+                if len(arr[i:]) == hint + 1:
                     arr[i] += 1
         i -= 1
     return arr
 
 
-# Problem for 6kyu - problem name `Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!`   
+# Problem for 6kyu - problem name `Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!`
 # https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/python
+
 
 def sum_dig_pow(a, b):
     """
@@ -46,15 +48,15 @@ def sum_dig_pow(a, b):
     """
     result = []
     counter = 1
-    
-    for x in range(a, b+1):
+
+    for x in range(a, b + 1):
         num = 0
 
         for digit in str(x):
-            num += int(digit)**counter
+            num += int(digit) ** counter
 
             counter += 1
-        if num==x:
+        if num == x:
             result.append(num)
 
         counter = 1
@@ -89,14 +91,13 @@ def n_nested(lst: list) -> int:
         int: Number of nested lists
     """
     n = 0
-    
+
     for value in lst:
         if isinstance(value, list):
             n += 1
             return n_nested(value)
         return 1
     print(value, n)
-        
 
 
 print(n_nested([2, [2, 3, [[[[[[[[5]]]]]]]]]]))
