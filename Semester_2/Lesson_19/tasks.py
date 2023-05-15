c85c631c0e677092a0ee4794075b1dddb8d3314d
inforgstr <xboyxmax@gmail.com>
2023-05-16 00:36:46 +0500

    deleted empty

--- Semester_2/Lesson_19/tasks.py
+++
@@ -1,8 +0,0 @@
-def n_nested(lst, nums=0):
-    for var in lst:
-        if isinstance(var, list):
-            nums += 1
-            return n_nested(var, nums)
-    return nums
-
-print(n_nested([2, [2, 3, [[[[[[[[5]]]]]]]]]]))
