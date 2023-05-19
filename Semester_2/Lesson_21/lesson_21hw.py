f67f1ba9eb6da94de177481569689134f73f21eb
inforgstr <xboyxmax@gmail.com>
2023-05-19 22:45:08 +0500

    completed authorize decorator with buy function for purchase

--- Semester_2/Lesson_21/lesson_21hw.py
+++ Semester_2/Lesson_21/lesson_21hw.py
@@ -14,3 +14,66 @@
 # print(num_nests(lst))
 
 
+data = {
+    "users": [
+        {
+            "username": "user_1",
+            "password": 1002030,
+        },
+        {
+            "username": "user_2",
+            "password": 1532124,
+        },
+        {
+            "username": "user_3",
+            "password": 5231423,
+        },
+    ],
+    "products": [
+        {
+            "name": "Phone",
+            "quantity": 30,
+            "price": 1000,
+        },
+        {
+            "name": "Laptop",
+            "quantity": 15,
+            "price": 2500,
+        },
+        {
+            "name": "Bag",
+            "quantity": 100,
+            "price": 200,
+        },
+    ],
+}
+
+
+def authorize(username, password):
+    def decorator_authorize(func):
+        def wrapper(*args, **kwargs):
+            for item in data["users"]:
+                if item["username"] == username and item["password"] == password:
+                    return func(*args, **kwargs)
+                else:
+                    return "Wrong credentials!\n\nPlease, check your data and resubmit your purchase!"
+
+        return wrapper
+
+    return decorator_authorize
+
+
+@authorize(username="user_1", password=1002030)
+def buy(name: str, amount: int, price: int) -> str:
+    for item in data["products"]:
+        if (
+            name == item["name"]
+            and price >= item["price"]
+            and amount <= item["quantity"]
+        ):
+            return f"Successful, for {name}! Your purchase has bought!\nYour remainder - {price-item['price']} $."
+        else:
+            return "Wrong credentials!\n\nPlease, check your data and resubmit your purchase!"
+
+
+print(buy("Phone", 30, 2000))
