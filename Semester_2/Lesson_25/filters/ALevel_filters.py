60db5fdd06023d714e085ac9b2bb5129cf309593
inforgstr <xboyxmax@gmail.com>
2023-05-28 23:20:40 +0500

    Changed saving mode: - Creates new directory to distribute json files for pleasant perceptive

---
+++ Semester_2/Lesson_25/filters/ALevel_filters.py
@@ -0,0 +1,76 @@
+import json
+import os
+
+from pathlib import Path
+
+
+def find_similars(data: list[dict]) -> str:
+    """
+    Find similar data based on tags and dump it into a JSON file.
+
+    Args:
+        data: List of dictionaries representing the data.
+
+    Returns:
+        A string indicating the status of the function.
+    """
+    similarities = []
+
+    # Iterate over each dictionary in the data list
+    for i in range(len(data)):
+        try:
+            # Split the tags string into a list and update the dictionary
+            data[i]["tags"] = data[i]["tags"].split(", ")
+
+            # Get the list of tags for the current dictionary
+            data_tags = data[i]["tags"]
+
+            # Initialize a counter to track the number of similarities
+            counter = 0
+
+            # Iterate over each tag in the list of tags
+            for tag in range(len(data_tags)):
+                # Iterate over each dictionary in the data list, excluding the current dictionary
+                for x in data[:i] + data[i + 1 :]:
+                    # Check if the current tag is present in the "tags" key of other dictionaries
+                    if data_tags[tag] in x["tags"]:
+                        counter += 1
+
+                # If there is at least one similarity and it is the last tag in the list,
+                # append a tuple with the similarity count and the current dictionary to the similarities list
+                if counter >= 1 and tag == len(data_tags) - 1:
+                    similarities.append((counter, data[i]))
+
+        except (TypeError, KeyError):
+            continue
+    # sorting with sorted function, that makes sortlist with most similtar value
+    similarities = sorted(similarities, key=lambda x: x[1]["published"])
+
+    # sorting for all similar posts
+    result = [x[1] for x in sorted(similarities, key=lambda x: x[0], reverse=True)]
+
+    # rewriting date into string, avoid JSON erros
+    for x in result:
+        x["published"] = str(x["published"])
+
+    try:
+        # create base variable to set path to main directory
+        base = Path(__file__).resolve().parent.parent
+
+        # check if path is available
+        try:
+            os.mkdir("%s/results/" % base)
+        except (OSError, FileExistsError):
+            pass
+
+        # saving result into json file
+        with open("%s/results/file.json" % base, "w") as file:
+            json.dump(result, file, indent=4)
+
+    except TypeError as te:
+        return (
+            "Please check your dictionary data, once you change it into string.\nException: %s"
+            % te
+        )
+    # return Done if the function completed its work correctly
+    return "Done!"
--- Semester_2/Lesson_25/lesson_25hw.py
+++ Semester_2/Lesson_25/lesson_25hw.py
@@ -1,5 +1,7 @@
 import datetime
-import json
+
+
+import filters.ALevel_filters as Alevel
 
 
 posts = [
@@ -83,67 +85,5 @@ posts = [
     },
 ]
 
-
-def find_similars(data: list[dict]) -> str:
-    """
-    Find similar data based on tags and dump it into a JSON file.
-
-    Args:
-        data: List of dictionaries representing the data.
-
-    Returns:
-        A string indicating the status of the function.
-    """
-    similarities = []
-
-    # Iterate over each dictionary in the data list
-    for i in range(len(data)):
-        try:
-            # Split the tags string into a list and update the dictionary
-            data[i]["tags"] = data[i]["tags"].split(", ")
-
-            # Get the list of tags for the current dictionary
-            data_tags = data[i]["tags"]
-
-            # Initialize a counter to track the number of similarities
-            counter = 0
-
-            # Iterate over each tag in the list of tags
-            for tag in range(len(data_tags)):
-                # Iterate over each dictionary in the data list, excluding the current dictionary
-                for x in data[:i] + data[i + 1 :]:
-                    # Check if the current tag is present in the "tags" key of other dictionaries
-                    if data_tags[tag] in x["tags"]:
-                        counter += 1
-
-                # If there is at least one similarity and it is the last tag in the list,
-                # append a tuple with the similarity count and the current dictionary to the similarities list
-                if counter >= 1 and tag == len(data_tags) - 1:
-                    similarities.append((counter, data[i]))
-
-        except (TypeError, KeyError):
-            continue
-    # sorting with sorted function, that makes sortlist with most similtar value
-    similarities = sorted(similarities, key=lambda x: x[1]["published"])
-
-    # sorting for all similar posts
-    result = [x[1] for x in sorted(similarities, key=lambda x: x[0], reverse=True)]
-
-    # rewriting date into string, avoid JSON erros
-    for x in result:
-        x["published"] = str(x["published"])
-
-    try:
-        # saving result into json file
-        with open("file.json", "w") as file:
-            json.dump(result, file, indent=4)
-    except TypeError as te:
-        return (
-            "Please check your dictionary data, once you change it into string.\nException: %s"
-            % te
-        )
-    # return Done if the function completed its work correctly
-    return "Done!"
-
-
-print(find_similars(posts))
+# Create json file which consists posts ordered by date and tag
+Alevel.find_similars(posts)
