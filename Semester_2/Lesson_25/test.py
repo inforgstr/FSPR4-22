78658fea2a522eab9d56c273d1c671ec67ff342b
inforgstr <xboyxmax@gmail.com>
2023-05-28 11:56:44 +0500

    deleted empty *py file

--- Semester_2/Lesson_25/test.py
+++
@@ -1,9 +0,0 @@
-lst = [2, 1, 10, 9]
-
-for x in range(len(lst)):
-    if x < len(lst) - 1:
-        if lst[x] > lst[x+1]:
-            lst[x] = lst[x+1]
-            lst[x+1] = lst[x]
-    
-print(lst)
