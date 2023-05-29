5ba6bfee24e40bb78d6bbf6c2a3754a822aa9a84
inforgstr <xboyxmax@gmail.com>
2023-05-29 20:49:48 +0500

    Added list of most similar tags name - It gives more opportunity for getting most popular tag from post. - Also added time counter to calculate implementation peiod.

--- Semester_2/Lesson_25/filters/ALevel_filters.py
+++ Semester_2/Lesson_25/filters/ALevel_filters.py
@@ -6,6 +6,36 @@ import time
 from pathlib import Path
 
 
+def save_json(result: list[dict], file_name="file", indent=4) -> None:
+    """
+    Save result list/dict to JSON file
+
+    Args:
+        result: Dictionaries of List
+        file_name: File name to save json file by this name
+        indent: amount of indents in json file
+
+    Returns:
+        None
+    """
+    try:
+        # create base variable to set path to main directory
+        base = Path(__file__).resolve().parent.parent
+
+        # check if path is available
+        try:
+            os.mkdir("%s/results/" % base)
+        except (OSError, FileExistsError):
+            pass
+
+        # saving result into json file
+        with open(f"{base}/results/{file_name}.json", "w") as file:
+            json.dump(result, file, indent=indent)
+
+    except TypeError:
+        pass
+
+
 def find_similars(data: list[dict]) -> str:
     """
     Find similar data based on tags and dump it into a JSON file.
@@ -59,44 +89,18 @@ def find_similars(data: list[dict]) -> str:
     for x in result:
         x["published"] = str(x["published"])
 
-    def save_json(result: list[dict]) -> None:
-        """
-        Save result list/dict to JSON file
-
-        Args:
-            result: Dictionaries of List
-
-        Returns:
-            None
-        """
-        try:
-            # create base variable to set path to main directory
-            base = Path(__file__).resolve().parent.parent
-
-            # check if path is available
-            try:
-                os.mkdir("%s/results/" % base)
-            except (OSError, FileExistsError):
-                pass
-
-            # saving result into json file
-            with open("%s/results/file.json" % base, "w") as file:
-                json.dump(result, file, indent=4)
-
-        except TypeError:
-            pass
-
-    # Call save_json function to save result
+    # Call save_json function to save result into JSON
     save_json(result)
 
     # If result not empty
     if result:
         # If there are results, print the count and the first tag
         print(
-            f"Have found {len(result)} on {most_similar_tags[0]} tag\n- {time.perf_counter()-start:8f} ms:\n"
+            f"Have found {len(result)} on {most_similar_tags[0]} tag\n- {time.perf_counter()-start:8f} seconds:\n"
         )
 
         for res in result:
+            time.sleep(0.8)
             # Extract the relevant information from each result
             title = res["title"]
             tag = res["tags"][1]
