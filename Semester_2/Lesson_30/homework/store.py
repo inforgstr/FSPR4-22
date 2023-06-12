4da9bc1237881fc213fa68e38e858ca6233227e2
inforgstr <xboyxmax@gmail.com>
2023-06-12 20:58:19 +0500

    Completed successfully tests for store class

---
+++ Semester_2/Lesson_30/homework/store.py
@@ -0,0 +1,135 @@
+import re
+
+
+USERS = [
+    {
+        "name": "Behruz",
+        "password": "234fjfdsd",
+        "email": "behruz@gmail.com",
+        "purchases": [],
+        "card": {"code": "3647583465734283", "balance": 1000},
+    },
+    {
+        "name": "Bob",
+        "password": "bob345345@",
+        "email": "bob@bob.com",
+        "purchases": [],
+        "card": {"code": "4567485767574876", "balance": 20},
+    },
+]
+PRODUCTS = [
+    {
+        "product_name": "sweater",
+        "count": 10,
+        "price": 100,
+        "color": "black",
+    },
+    {
+        "product_name": "bag",
+        "count": 20,
+        "price": 200,
+        "color": "gray",
+    },
+]
+
+
+class Store:
+    purchases = []
+
+    def __init__(self, name, email, password, card_code, card_balance):
+        self.name = name
+        self.password = password
+        self.email = email
+        self.card_code = card_code
+        self.card_balance = card_balance
+
+    @classmethod
+    def register(cls, name, email, password, card_code, card_balance):
+        for user in USERS:
+            if email == user["email"] and user["password"] == password:
+                return "User with this email or password is already in exists."
+
+        if not (name and email and password and card_balance and card_code):
+            return "Empty values were given."
+
+        if (
+            isinstance(name, str)
+            and re.match(r"^[A-Z]\w+$", name)
+            and re.match(r"^[\w.]+@[\w.]+$", email)
+            and re.match(r"^[\w@#$]{6,16}$", password)
+            and re.match(r"^\d{16}$", card_code)
+            and re.match(r"^\d+$", str(card_balance))
+        ):
+            USERS.append(
+                {
+                    "name": name,
+                    "password": password,
+                    "email": email,
+                    "purchases": [],
+                    "card": {"code": card_code, "money": int(card_balance)},
+                }
+            )
+            return cls(name, email, password, card_code, card_balance)
+
+        return "Wrong credentials."
+
+    @classmethod
+    def login(cls, email, password):
+        if not (email and password):
+            return "Empty values were given."
+        for user in USERS:
+            if user["email"] == email and password == user["password"]:
+                return cls(
+                    user["name"],
+                    email,
+                    password,
+                    user["card"]["code"],
+                    user["card"]["balance"],
+                )
+
+        return "Wrong email or password"
+
+    def purchase(self, product, amount=1):
+        card_balance = int(self.card_balance)
+        try:
+            amount = int(amount)
+        except ValueError as error:
+            return error
+
+        if card_balance <= 0:
+            return "Not enough money."
+
+        product_dict = {commodity["product_name"]: commodity for commodity in PRODUCTS}
+
+        if product not in product_dict:
+            return "Not available."
+
+        commodity = product_dict[product]
+        price = int(commodity["price"])
+        total_cost = price * amount
+
+        if card_balance < total_cost:
+            return "Not enough money."
+
+        self.card_balance -= total_cost
+        product_list = [product] * amount
+        self.purchases += product_list
+
+        user_lst = False
+
+        for id, user in enumerate(USERS):
+            if user["email"] == self.email and user["password"] == self.password:
+                user_lst = True
+                USERS[id]["purchases"] += product_list
+
+        if not user_lst:
+            return "User not found."
+
+        commodity["count"] -= amount
+        return "Success."
+
+    def user_exists(self):
+        for user in USERS:
+            if self.email == user["email"] and self.password == user["password"]:
+                return True
+        return False
---
+++ Semester_2/Lesson_30/homework/test_store.py
@@ -0,0 +1,120 @@
+import unittest
+import store
+
+
+class TestStore(unittest.TestCase):
+    def setUp(self):
+        self.store = store.Store
+
+    # Tests for login method in Store class
+    def test_store_login_success_user_exists(self):
+        login_email = "behruz@gmail.com"
+        login_password = "234fjfdsd"
+
+        result = self.store.login(login_email, login_password)
+        self.assertTrue(result.user_exists())
+
+    def test_store_login_empty_password_error(self):
+        login_email = "behruz@gmail.com"
+        login_password = None
+
+        result = self.store.login(login_email, login_password)
+        self.assertEqual(result, "Empty values were given.")
+
+    def test_store_login_empty_args(self):
+        login_email = None
+        login_password = None
+
+        result = self.store.login(login_email, login_password)
+        self.assertEqual(result, "Empty values were given.")
+
+    def test_store_login_fail(self):
+        login_email = "sdlfkjlkdfj@gmail.com"
+        login_password = "2345lsdfj"
+
+        result = self.store.login(login_email, login_password)
+        self.assertEqual(result, "Wrong email or password")
+
+    # Tests for register method in Store class
+    def test_store_register_success_user_exists(self):
+        name = "Emma"
+        email = "emma@emma.com"
+        password = "sldkfj345@kj"
+        card_code = "8576854858685845"
+        card_balance = 1000
+
+        result = self.store.register(name, email, password, card_code, card_balance)
+        self.assertTrue(result.user_exists())
+
+    def test_store_register_exists_user_error(self):
+        name = "John"
+        email = "behruz@gmail.com"
+        password = "234fjfdsd"
+        card_code = "8576857689485768"
+        card_balance = 10339
+
+        result = self.store.register(name, email, password, card_code, card_balance)
+        self.assertEqual(
+            result, "User with this email or password is already in exists."
+        )
+
+    def test_store_register_field_fail(self):
+        name = "johj"
+        email = "dlkjfskldjf@gmail.com"
+        password = "slkdfjs23jsldf"
+        card_code = "8697857475867324"
+        card_balance = 10029
+
+        result = self.store.register(name, email, password, card_code, card_balance)
+        self.assertEqual(result, "Wrong credentials.")
+
+    def test_store_register_empty_value(self):
+        name = None
+        email = "email@email.com"
+        password = "sldkjf384@"
+        card_code = "3948585849384859"
+        card_balance = 12094
+
+        result = self.store.register(name, email, password, card_code, card_balance)
+        self.assertEqual(result, "Empty values were given.")
+
+    # Tests for purchase method in Store class
+    def test_store_purchase_success_with_login_available_product(self):
+        user = self.store.login("behruz@gmail.com", "234fjfdsd")
+        result = user.purchase("bag", 4)
+        self.assertEqual(result, "Success.")
+
+    def test_store_purchase_product_amount_error_with_login(self):
+        user = self.store.login("behruz@gmail.com", "234fjfdsd")
+        result = user.purchase("bag", "two")
+        self.assertIsInstance(result, ValueError)
+
+    def test_store_purchase_with_login_not_enough_balance_insufficient_funds(self):
+        user = self.store.login("bob@bob.com", "bob345345@")
+
+        result = user.purchase("sweater", 2)
+        self.assertEqual(result, "Not enough money.")
+
+    def test_store_purchase_product_invalid_with_login(self):
+        user = self.store.login("behruz@gmail.com", "234fjfdsd")
+        result = user.purchase("Jh", 2)
+        self.assertEqual(result, "Not available.")
+
+    def test_store_purchase_product_amount_not_given(self):
+        user = self.store.login("behruz@gmail.com", "234fjfdsd")
+        user.purchase("sweater")
+
+        self.assertEqual(len(user.purchases), 1)
+
+    def test_store_purchase_user_not_found(self):
+        user = self.store("Emma", "emma@emma.com", "ldskjf83#45", "8576857689878767", 300)
+        result = user.purchase("bag", 1)
+
+        self.assertEqual(result, "User not found.")
+
+    def tearDown(self):
+        self.store = None
+
+
+if __name__ == "__main__":
+    unittest.main()
