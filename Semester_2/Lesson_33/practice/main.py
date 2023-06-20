7de80506c43f030b1f8d4f917469e2b6134313f0
inforgstr <xboyxmax@gmail.com>
2023-06-20 17:22:39 +0500

    committed

--- Semester_2/Lesson_33/practice/main.py
+++ Semester_2/Lesson_33/practice/main.py
@@ -1,4 +1,3 @@
-import os
 import smtplib
 import ssl
 
@@ -10,33 +9,37 @@ from string import Template
 from el import email_reciever
 
 
-email_sender = os.environ.get("email")
-email_password = os.environ.get("email_password")
+email_senders = {
+    "sdlkjflkdsj@gmail.com": "password1",
+    "slkdjflksjdf@gmail.com": "password2",
+    "slkdjlkdsjijei@gmail.com": "password3",
+}
 
+
+html = Template((Path(__file__).parent / "index.html").read_text())
 
 message = MIMEMultipart("alternative")
-message["Subject"] = "Crypto"
-message["From"] = email_sender
-message["To"] = email_reciever
 
 
-html = Template((Path(__file__).parent/"index.html").read_text())
+for email_sender, email_password in email_senders:
+    message["Subject"] = "Crypto"
+    message["From"] = email_sender
+    message["To"] = email_reciever
 
-part = MIMEText(
-    html.substitute(
-        {
-            "btc_value": 25563,
-        }
-    ),
-    "html",
-)
-message.attach(part)
+    part = MIMEText(
+        html.substitute(
+            {
+                "btc_value": 25563,
+            }
+        ),
+        "html",
+    )
+    message.attach(part)
 
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    print("Connecting to server...")
-    server.login(email_sender, email_password)
-    print("Logging...")
-    server.sendmail(email_sender, email_reciever, message.as_string())
-    print("Email was sent!")
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        print("Connecting to server...")
+        server.login(email_sender, email_password)
+        print("Logging...")
+        server.sendmail(email_sender, email_reciever, message.as_string())
+        print("Email was sent!")
